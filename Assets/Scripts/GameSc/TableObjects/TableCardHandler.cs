424fff850fbcf693901769bb2dfbb0f3d2908fac
DongHyun96 <wadanka09@gmail.com>
2022-05-17 00:21:13 +0900

    Finish Card animations and Scripting

--- Assets/Scripts/GameSc/TableObjects/TableCardHandler.cs
+++ Assets/Scripts/GameSc/TableObjects/TableCardHandler.cs
@@ -10,76 +10,145 @@ public class TableCardHandler : MonoBehaviour
     [SerializeField] private List<GameObject> communityCards;
     [SerializeField] private List<GameObject> discards;
 
-    public void DrawFirstCard(int idx, Card c)
+    public enum DrawType{
+        FIRST, SECOND, COMMUNITY, DISCARD
+    }
+
+    public void DrawCard(DrawType type, int idx, Card c)
     {
-        /* Destroy child if it has one */
-        for(int i = 0; i < playerFirstCards[idx].transform.childCount; i++)
+        /* Get corresponding anim trigger and card list */
+        string trigger = "";
+        List<GameObject> cardList = GetCorrespondingCardList(type, out trigger);
+
+        /* Set Card */
+        GameObject prefab = CardHolder.GetInstance().GetCardPrefab(c);
+        GameObject container = Instantiate(prefab, prefab.transform.position, prefab.transform.rotation);
+        SetParent(container.transform, cardList[idx].transform);
+
+        /* Play anim */
+        Animator anim = cardList[idx].GetComponent<Animator>();
+        anim.SetInteger("index", idx);
+        anim.SetTrigger(trigger);
+    }
+
+    public void FlipPlayerCard(int pIdx, int cardIdx)
+    {
+        /* Get corresponding card object */
+        GameObject targetCard = pIdx == 0 ? playerFirstCards[cardIdx] : playerSecondCards[cardIdx];
+
+        /* Play anim */
+        Animator anim = targetCard.GetComponent<Animator>();
+        anim.SetTrigger("flip");
+    }
+
+    /* To do - Delete gameObject after animation finished */
+    /* Do this by animation events */
+    public void GatherCardsToDeck()
+    {
+        foreach(GameObject obj in playerFirstCards)
         {
-            Destroy(playerFirstCards[idx].transform.GetChild(i).gameObject);
+            Animator anim = obj.GetComponent<Animator>();
+            anim.SetTrigger("goToDeck");
+        }
+        // foreach(GameObject obj in )
+        for(int i = 0; i < playerFirstCards.Count; i++)
+        {
+            Animator firstCardAnim = playerFirstCards[i].GetComponent<Animator>();
+            Animator secondCardAnim = playerSecondCards[i].GetComponent<Animator>();
+
+            firstCardAnim.SetTrigger("goToDeck");
+            secondCardAnim.SetTrigger("goToDeck");
+        }
+        foreach(GameObject obj in discards)
+        {
+            Animator anim = obj.GetComponent<Animator>();
+            anim.SetTrigger("goToDeck");
+        }
+        foreach(GameObject obj in communityCards)
+        {
+            Animator anim = obj.GetComponent<Animator>();
+            anim.SetTrigger("goToDeck");
         }
 
-        /* Set card */
-        GameObject prefab = CardHolder.GetInstance().GetCardPrefab(c);
-        GameObject container = Instantiate(prefab);
-        SetParent(container.transform, playerFirstCards[idx].transform);
-
-        /* Play first card giving anim */
-
-    }
-
-    public void DrawSecondCard(int idx, Card c)
-    {
-        /* Set card */
-        /* Play second card giving anim */
-    }
-
-    public void DrawCommunityCard(int idx, Card c)
-    {
-        /* Set card */
-        /* Play */
-    }
-
-    public void Discard(int idx)
-    {
-        /* Play */
     }
 
     private void Start() {
-        /* Card c = new Card(Card.Suit.CLUB, 5);
-        GameObject prefab = CardHolder.GetInstance().GetCardPrefab(c);
-        GameObject container = Instantiate(prefab);
-        SetParent(container.transform, playerFirstCards[0].transform);
-
-        Animator anim = playerFirstCards[0].GetComponent<Animator>();
-        StartCoroutine(example(anim)); */
+        StartCoroutine(example());
     }
 
     private void SetParent(Transform child, Transform parent)
     {
         child.parent = parent;
         child.localPosition = Vector3.zero;
-        child.localRotation = Quaternion.identity;
-        child.localScale = Vector3.one;
+        // child.localScale = 20 * Vector3.one;
     }
 
-    private IEnumerator example(Animator anim)
+    private IEnumerator example()
     {
         yield return new WaitForSeconds(7.0f);
-        anim.SetInteger("index", 0);
-        anim.SetTrigger("first");
-        yield return new WaitForSeconds(3.0f);
-        anim.SetTrigger("goToDeck");
-        yield return new WaitForSeconds(3.0f);
-        anim.SetInteger("index", 3);
-        anim.SetTrigger("second");
+        Card c = new Card(Card.Suit.HEART, 12);
+        /* Draw cards to player test */
+        DrawCard(DrawType.DISCARD, 0, c);
+        for(int i = 0; i < 6; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            DrawCard(DrawType.FIRST, i, c);
+        }
+        for(int i = 0; i < 6; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            DrawCard(DrawType.SECOND, i, c);
+        }
 
+        yield return new WaitForSeconds(2.0f);
+
+        /* Draw cards to community test */
+        DrawCard(DrawType.DISCARD, 1, c);
+        for(int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            DrawCard(DrawType.COMMUNITY, i, c);
+        }
         yield return new WaitForSeconds(3.0f);
 
-        anim.SetTrigger("goToDeck");
-        yield return new WaitForSeconds(3.0f);
-        
-        anim.SetInteger("index", 0);
-        anim.SetTrigger("community");
+        /* Flip players card */
+        for(int i = 0; i < 6; i++)
+        {
+            FlipPlayerCard(0, i);
+            FlipPlayerCard(1, i);
+            yield return new WaitForSeconds(0.3f);
+        }
+
+        yield return new WaitForSeconds(5.0f);
+        /* Gather cards to deck */
+        GatherCardsToDeck();
+
+    }
+
+    private List<GameObject> GetCorrespondingCardList(DrawType t, out string trigger)
+    {
+        switch(t)
+        {
+            case DrawType.FIRST:
+                trigger = "first";
+                return playerFirstCards;
+            case DrawType.SECOND:
+                trigger = "second";
+                return playerSecondCards;
+            case DrawType.COMMUNITY:
+                trigger = "community";
+                return communityCards;
+            case DrawType.DISCARD:
+                trigger = "discard";
+                return discards;
+            default:
+                trigger = "";
+                return null;
+        }
+    }
+
+    public void OnFinishGathering()
+    {
 
     }
 }
